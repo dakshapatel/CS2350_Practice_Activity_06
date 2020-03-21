216ed878009cb00f98d63b189f529be5068e6f51
Daksha <dakshapatel100@gmail.com>
2020-03-20 22:40:11 -0600

    simple form with display method

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -1,7 +1,18 @@
 // Required by Webpack - do not touch
-require.context('../', true, /\.(html|json|txt|dat)$/i)
-require.context('../images/', true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i)
-require.context('../stylesheets/', true, /\.(css|scss)$/i)
+// Webpack compiles all the files into a single file
+require.context("../", true, /\.(html|json|txt|dat)$/i);
+// compile all images files into a single image file
+require.context("../images/", true, /\.(gif|jpg|png|svg|eot|ttf|woff|woff2)$/i);
+// compile all .css files into a single css file called application.css
+require.context("../stylesheets/", true, /\.(css|scss)$/i);
 
 // JavaScript
 //TODO
+import "bootstrap";
+
+
+function displayMessage() {
+    let name;
+  alert("Welcome" + name + "!");
+  return true;
+}
